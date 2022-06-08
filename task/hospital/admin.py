6e92598fd8e26ac7fc31ccdc276ef8ca99477345
Shakyawar-Nishank <92313470+Shakyawar-Nishank@users.noreply.github.com>
2022-06-08 14:08:17 +0530

    Add files via upload

---
+++ task/hospital/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import hospital
+
+admin.site.register(hospital)
+
+
+# Register your models here.
---
+++ task/hospital/models.py
@@ -0,0 +1,23 @@
+from email.headerregistry import Address
+from django.db import models
+from django.contrib.auth.models import User
+
+
+USER_TYPES = (
+    ('PAT', 'Patient'),
+    ('DOC', 'Doctor'),
+)
+
+
+class hospital(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(
+        max_length=200, choices=USER_TYPES, blank=False, null=False)
+    profile_image = models.ImageField(blank=True, null=True, upload_to='')
+    address_line_1 = models.CharField(
+        max_length=500, blank=False, null=False)
+    city = models.CharField(
+        max_length=200, blank=False, null=False)
+    state = models.CharField(
+        max_length=200, blank=False, null=False)
+    pincode = models.CharField(max_length=6, blank=False, null=False)
